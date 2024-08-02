47402405e11ece8c2d2760323b83628f2912d052
Alton999 <alton_ong@live.com.sg>
2024-08-02 10:18:03 +1000

    remove reevaluator prisma

--- app/api/generate/reinitiator/route.ts
+++ app/api/generate/reinitiator/route.ts
@@ -25,43 +25,46 @@ export async function POST(req: Request, res: Response) {
 				error: "Prompt instance not found"
 			});
 		}
-		// Lets see if there is already an instance of search results in the database
-		const existingReinitiation = await prisma.reinitiator.findFirst({
-			where: {
-				promptId: hypothesisEvaluationInstance.promptId
-			}
+		return NextResponse.json({
+			message: "Prompt instance not found"
 		});
+		// Lets see if there is already an instance of search results in the database
+		// const existingReinitiation = await prisma.reinitiator.findFirst({
+		// 	where: {
+		// 		promptId: hypothesisEvaluationInstance.promptId
+		// 	}
+		// });
 
 		// If instance exists return that instance else create new instance
-		if (existingReinitiation) {
-			console.log("Search result hypothesis exists:", existingReinitiation);
-			return NextResponse.json({
-				reinitiation: existingReinitiation
-			});
-		} else {
-			console.log("Creating new evaluation instance");
+		// if (existingReinitiation) {
+		// 	console.log("Search result hypothesis exists:", existingReinitiation);
+		// 	return NextResponse.json({
+		// 		reinitiation: existingReinitiation
+		// 	});
+		// } else {
+		// 	console.log("Creating new evaluation instance");
 
-			const reinitiation = await reinitiator({
-				evaluation: hypothesisEvaluationInstance.evaluation,
-				wordwarePromptId: "3e8aa0e0-ecc3-4f90-8024-713ca293583e",
-				researchQuestion: promptInstance.researchQuestion
-			});
-			const reinitiatorInstance = await prisma.reinitiator.create({
-				data: {
-					promptId: hypothesisEvaluationInstance.promptId,
-					hypothesisId: hypothesisEvaluationInstance.hypothesisId,
-					searchTermId: hypothesisEvaluationInstance.searchTermId,
-					searchResultId: hypothesisEvaluationInstance.searchResultId,
-					hypothesisEvaluationId: hypothesisEvaluationInstance.id,
-					reinitiation: reinitiation
-				}
-			});
+		// 	const reinitiation = await reinitiator({
+		// 		evaluation: hypothesisEvaluationInstance.evaluation,
+		// 		wordwarePromptId: "3e8aa0e0-ecc3-4f90-8024-713ca293583e",
+		// 		researchQuestion: promptInstance.researchQuestion
+		// 	});
+		// 	const reinitiatorInstance = await prisma.reinitiator.create({
+		// 		data: {
+		// 			promptId: hypothesisEvaluationInstance.promptId,
+		// 			hypothesisId: hypothesisEvaluationInstance.hypothesisId,
+		// 			searchTermId: hypothesisEvaluationInstance.searchTermId,
+		// 			searchResultId: hypothesisEvaluationInstance.searchResultId,
+		// 			hypothesisEvaluationId: hypothesisEvaluationInstance.id,
+		// 			reinitiation: reinitiation
+		// 		}
+		// 	});
 
-			// 	// console.log("Hypothesis generation:", hypothesisGenerationInstance);
-			return NextResponse.json({
-				reinitiation: reinitiatorInstance
-			});
-		}
+		// 	// 	// console.log("Hypothesis generation:", hypothesisGenerationInstance);
+		// 	return NextResponse.json({
+		// 		reinitiation: reinitiatorInstance
+		// 	});
+		// }
 	} catch (error: any) {
 		console.error("Error generating evaluation");
 		console.error("Error message:", error.message);
